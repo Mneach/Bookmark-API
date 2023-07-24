d38361299513010828a06a36fde681ae7b4a9e09
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2023-07-24 13:02:24 +0700

    add end to end testing for all api

---
+++ src/user/dto/edit-user.dto.ts
@@ -0,0 +1,14 @@
+import { IsEmail, IsOptional, IsString } from "class-validator"
+
+export class EditUserDto{
+
+    @IsEmail()
+    email? : string
+
+    @IsString()
+    firstName? : string
+
+    @IsString()
+    @IsOptional()
+    lastName? : string
+}
